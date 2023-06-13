9645dab5f5b0130392c463445ba7a5097dca0cef
KExa <64263737+KExa-T@users.noreply.github.com>
2023-06-13 23:00:36 +0800

    Control_reader.java

--- Control_reader.java
+++ Control_reader.java
@@ -673,7 +673,7 @@ public class Control_reader {
 	public void getStatement() throws Exception
 	{
 		String root="root";
-		String pass="513254687";
+		String pass="";
 		conn=DriverManager.getConnection("jdbc:mysql:///bookBms",root,pass);
 		stmt=conn.createStatement();
 	}
