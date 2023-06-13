c668ba13a5d5c9b0f7f8af496be662230d673df9
KExa <64263737+KExa-T@users.noreply.github.com>
2023-06-13 23:01:23 +0800

    Control_admin.java

--- Control_admin.java
+++ Control_admin.java
@@ -882,7 +882,7 @@ public class Control_admin {
 	public void getStatement() throws Exception
 	{
 		String root="root";
-		String pass="513254687";
+		String pass="";
 		conn=DriverManager.getConnection("jdbc:mysql:///bookBms",root,pass);
 		stmt=conn.createStatement();
 	}
